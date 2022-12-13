9cc3770057c31c0962ea5fa938d24c1791805d28
Patrick Thebeau <a07x@outlook.com>
2022-12-13 15:13:29 -0500

    fix

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/auth', methods=['GET'])
 def get_authorization():
     #this url works to get the user to authorize data access and goes to yap.html with an appended code...not token exchange is done yet
-    return redirect("https://api.login.yahoo.com/oauth2/request_auth?client_id=dj0yJmk9bVJqTU1ob1F0WEpnJmQ9WVdrOVMwSkVia05RVEVrbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTZj&redirect_uri=https://thebeau.ca/yap/yap.html&response_type=code&language=en-us")
+    return redirect("https://api.login.yahoo.com/oauth2/request_auth?client_id=dj0yJmk9bVJqTU1ob1F0WEpnJmQ9WVdrOVMwSkVia05RVEVrbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTZj&redirect_uri=https://thebeau.dev/yap/yap.html&response_type=code&language=en-us")
 
 @app.route('/get_token/<redirect_uri>', methods=['GET'])
 def get_token():
