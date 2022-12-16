a9fb5bd32b4bd755a1133cd8980325b5136f29c7
Patrick Thebeau <a07x@outlook.com>
2022-12-16 14:12:33 -0500

    update and addition for testing

--- app.py
+++ app.py
@@ -1,44 +1,26 @@
-from flask import Flask, redirect, jsonify
+from flask import Flask, redirect, jsonify, request, json
 from flask_cors import CORS, cross_origin
-import requests
+
+import os, time
+from rauth import OAuth2Service
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 #YAHOO AUTH FLOW
-@app.route('/auth', methods=['GET'])
-def get_authorization():
-    #this url works to get the user to authorize data access and goes to yap.html with an appended code...not token exchange is done yet
-    return redirect("https://api.login.yahoo.com/oauth2/request_auth?client_id=dj0yJmk9bVJqTU1ob1F0WEpnJmQ9WVdrOVMwSkVia05RVEVrbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTZj&redirect_uri=https://thebeau.dev/yap/yap.html&response_type=code&language=en-us")
-
-@app.route('/authurl', methods=['GET'])
-def get_authurl():
-    authurl = {
-        'authurl': "https://api.login.yahoo.com/oauth2/request_auth?client_id=dj0yJmk9bVJqTU1ob1F0WEpnJmQ9WVdrOVMwSkVia05RVEVrbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTZj&redirect_uri=https://thebeau.dev/yap/yap.html&response_type=code&language=en-us",
-        }
-    return jsonify(authurl)
-
-@app.route('/appcreds', methods=['GET'])
-def get_appcreds():
-    creds = {
-        'clientid': 'dj0yJmk9bVJqTU1ob1F0WEpnJmQ9WVdrOVMwSkVia05RVEVrbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTZj',
-        'redirect_uri': 'https://thebeau.dev/yap/yap.html',
-        }
-    return jsonify(creds)
+@app.route('/get_key', method=['GET'])
+def get_key():
+    f = open('creds.json')
+    creds = json.load(f)
+    return creds['consumer_key']
 
 @app.route('/get_token', methods=['GET'])
 def get_token():
-    tokenurl = {
-        'tokenurl' : 'https://api.login.yahoo.com/oauth2/get_token?client_id=dj0yJmk9bVJqTU1ob1F0WEpnJmQ9WVdrOVMwSkVia05RVEVrbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTZj?client_secret=556577b5e9ece81e03edea4f5baf2b0fdfe432e7?grant_type=authorization_code?code='+requests.args.get('code')+'?redirect_uri=https://thebeau.dev/yap/yap.html',
-    }
-    return jsonify(tokenurl)
-
-        #"client_id": 'dj0yJmk9bVJqTU1ob1F0WEpnJmQ9WVdrOVMwSkVia05RVEVrbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTZj',
-        #"client_secret": '556577b5e9ece81e03edea4f5baf2b0fdfe432e7',
-        #"grant_type": "authorization_code",
-        #"code": requests.args.get('code'),
-        #"redirect_uri": "https://thebeau.dev/yap/yap.html",
+    f = open('creds.json')
+    creds = json.load(f)
+    auth_code = request.args.get('auth_code')
+    return auth_code
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8000, debug=True)
---
+++ test.py
@@ -0,0 +1,129 @@
+import os
+import time
+import json
+
+from rauth import OAuth2Service
+
+class ClientKey(object):
+    def __init__(self, client_id, client_secret):
+        self.client_id = client_id
+        self.client_secret = client_secret
+
+
+class Token(object):
+    def __init__(self, access_token=None, refresh_token=None):
+        self.access_token = access_token
+        self.refresh_token = refresh_token
+        self.expiration_time = 0
+
+    @property
+    def expires_in(self):
+        return self.expiration_time - time.time()
+
+    @property
+    def is_expired(self):
+        return self.expires_in <= 0
+
+    def get(self, oauth_service):
+        if self.refresh_token:
+            data = {
+                "refresh_token": self.refresh_token,
+                "redirect_uri": "oob",
+                "grant_type": "refresh_token",
+            }
+        else:
+            data = {
+                "code": self._get_code(oauth_service),
+                "redirect_uri": "oob",
+                "grant_type": "authorization_code",
+            }
+
+        self._get_token(oauth_service, data)
+
+    def _get_code(self, oauth_service):
+        params = {
+            "redirect_uri": "oob",
+            "response_type": "code",
+        }
+        authorize_url = oauth_service.get_authorize_url(**params)
+
+        print("Sign in here: " + str(authorize_url))
+        return input("Enter code: ")
+
+    def _get_token(self, oauth_service, data):
+        raw_token = oauth_service.get_raw_access_token(data=data)
+
+        parsed_token = raw_token.json()
+        self.access_token = parsed_token["access_token"]
+        self.refresh_token = parsed_token["refresh_token"]
+        self.expiration_time = time.time() + parsed_token["expires_in"]
+
+    @classmethod
+    def from_file(cls, token_file):
+        with open(token_file, "r") as f:
+            token = f.read().strip()
+
+        if len(token.splitlines()) != 1:
+            raise RuntimeError("Incorrect token format")
+
+        return cls(refresh_token=token)
+
+    def save(self, token_file):
+        with open(token_file, "w") as f:
+            f.write(self.refresh_token)
+
+
+class YahooAPI(object):
+    def __init__(self, base_url="https://fantasysports.yahooapis.com",
+                 request_period=0):
+
+        f = open('creds.json')
+        creds = json.load(f)
+
+        self.key = creds['consumer_key']
+        self.token = Token()
+
+        self.oauth = OAuth2Service(
+            client_id=creds['consumer_key'],
+            client_secret=creds['consumer_secret'],
+            name="yahoo",
+            authorize_url="https://api.login.yahoo.com/oauth2/request_auth",
+            access_token_url="https://api.login.yahoo.com/oauth2/get_token",
+            base_url=base_url,
+        )
+
+        self.session = None
+
+        self._update_token()
+
+        self.session = self.oauth.get_session(self.token.access_token)
+
+        self.last_request = time.time()
+        self.request_period = request_period
+
+    def _update_token(self):
+        self.token.get(self.oauth)
+
+        if self.tokenfile:
+            self.token.save(self.tokenfile)
+
+        if self.session:
+            self.session.access_token = self.token.access_token
+
+    def request(self, request_str, params={}):
+        """get json instead of xml like this params={'format': 'json'}"""
+
+        tdiff = max(0, time.time() - self.last_request)
+        if tdiff >= 0 and tdiff < self.request_period:
+            time.sleep(self.request_period - tdiff)
+
+        self.last_request = time.time()
+
+        # refresh access token 60 seconds before it expires
+        if self.token.expires_in < 60:
+            self._update_token()
+
+        return self.session.get(url=request_str, params=params)
+
+
+YahooAPI().__init__
